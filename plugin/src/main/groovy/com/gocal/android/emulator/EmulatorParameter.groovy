6d90d5c61dd2850a77684a445d0278511c387e30
Marcin Marek Gocał <marek@up-next.com>
2016-04-13 17:21:13 +0200

    add tools switch in travis config

--- plugin/src/main/groovy/com/gocal/android/emulator/EmulatorParameter.groovy
+++ plugin/src/main/groovy/com/gocal/android/emulator/EmulatorParameter.groovy
@@ -5,7 +5,8 @@ enum EmulatorParameter {
     LIST_AVDS("list-avds"),
     AVD("avd"),
     VERSION("version"),
-    NO_WINDOW("no-window")
+    NO_WINDOW("no-window"),
+    NO_AUDIO("no-audio"),
 
     private String value;
 
