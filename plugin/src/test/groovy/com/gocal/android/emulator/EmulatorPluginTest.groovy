c637cca77eb147a0d41d9e32b0778a8c6f6daaba
Marek Gocal <marcin.gocal@gmail.com>
2016-04-19 21:16:50 +0200

    testing gradle plugin

--- plugin/src/test/groovy/com/gocal/android/emulator/EmulatorPluginTest.groovy
+++ plugin/src/test/groovy/com/gocal/android/emulator/EmulatorPluginTest.groovy
@@ -1,123 +1,45 @@
 package com.gocal.android.emulator
 
 import com.gocal.android.emulator.avd.Avd
-import com.gocal.android.emulator.utils.TestUtils
-import org.apache.commons.io.FileUtils
 import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.Copy
 import org.gradle.testfixtures.ProjectBuilder
-import org.gradle.tooling.BuildLauncher
-import org.gradle.tooling.GradleConnector
-import org.gradle.tooling.model.GradleProject
-import org.junit.Test
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
 
 class EmulatorPluginTest {
 
-    //@Test
-    public void pluginTest() {
-        def projectDir = new File('..\\sample')
-        Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
-        project.getPlugins().apply 'com.android.application'
-        def plugin = new EmulatorPlugin()
-        plugin.apply(project)
+    private Project project
+    private static final PLUGIN_ID = 'android-emulator'
 
-        EmulatorExtension emulatorExtension = project.extensions.findByType(EmulatorExtension)
+    @BeforeMethod
+    public void setUp() throws Exception {
+        project = ProjectBuilder.builder().build()
+        project.buildscript {
 
-        if(emulatorExtension != null) {
-
-        }
-
-        def connector = GradleConnector.newConnector().forProjectDirectory(projectDir)
-        def connection = connector.connect()
-        try {
-
-            BuildLauncher builder = connection.newBuild()
-            def model = connection.getModel(GradleProject)
-
-            if(model != null) {
-
+            repositories {
+                jcenter()
             }
 
-        } finally {
-            connection?.close()
+            dependencies {
+                classpath 'org.jacoco:org.jacoco.core:0.7.4.201502262128'
+                classpath 'com.android.tools.build:gradle:2.0.0'
+            }
         }
-    }
 
+        project.apply plugin: 'com.android.application'
 
-    //@Test
-    public void startEmulator() {
-        def projectDir = new File('..\\sample')
-        Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
-        project.getPlugins().apply 'com.android.application'
-        def androidExtension = project.android;
-        def androidHome = TestUtils.getAndroidHome(androidExtension)
+        def androidApplication = project.plugins.getPlugin('com.android.application')
+        androidApplication.sdkHandler.sdkFolder = new File("C:\\Users\\marek\\dev\\tools\\android-sdk")
 
-        def emulatorFile = FileUtils.getFile(androidHome, "tools", "emulator.exe")
-        def adbFile = FileUtils.getFile(androidHome, "platform-tools", "adb.exe")
-        def emulator = new AndroidEmulator(emulatorFile.absolutePath, adbFile.absolutePath)
-
-        Avd avd = new Avd()
-        avd.name = "marek"
-
-        emulator.setAvd(avd)
-
-        emulator.start()
-    }
-
-    //@Test
-    public void stopEmulator() {
-        def projectDir = new File('..\\sample')
-        Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
-        project.getPlugins().apply 'com.android.application'
-        def androidExtension = project.android;
-        def androidHome = TestUtils.getAndroidHome(androidExtension)
-
-        def emulatorFile = FileUtils.getFile(androidHome, "tools", "emulator.exe")
-        def adbFile = FileUtils.getFile(androidHome, "platform-tools", "adb.exe")
-        def emulator = new AndroidEmulator(emulatorFile.absolutePath, adbFile.absolutePath)
-        emulator.stop()
+        project.apply plugin: 'android-emulator'
     }
 
     @Test
-    public void testStartEmulator() {
-        /*
-        def projectDir = new File('..\\sample')
-
-        def connector = GradleConnector.newConnector().forProjectDirectory(projectDir)
-        def connection = connector.connect()
-        try {
-
-            BuildLauncher builder = connection.newBuild()
-            def buildLaunch = builder.forTasks("startEmulator")
-            buildLaunch.run()
-            def model = connection.getModel(GradleProject)
-
-        } finally {
-            connection?.close()
-        }
-
-        def projectDir = new File('..\\sample')
-        Project project = ProjectBuilder.builder().withProjectDir(projectDir).build()
-        project.getPlugins().apply 'com.android.application'
-        def androidExtension = project.android;
-        def androidHome = getAndroidHome(androidExtension)
-
-        def emulatorFile = FileUtils.getFile(androidHome, "tools", "emulator.exe")
-        def adbFile = FileUtils.getFile(androidHome, "platform-tools", "adb.exe")
-        def emulator = new AndroidEmulator(emulatorFile.absolutePath, adbFile.absolutePath)
-        def version = emulator.version
-              */
-
-    }
-
-    private static String getAndroidHome(androidExtension) {
-        def androidHome
-        if (androidExtension.hasProperty('sdkHandler')) {
-            androidHome = "${androidExtension.sdkHandler.sdkFolder}"
-        } else if (androidExtension.hasProperty('sdkDirectory')) {
-            androidHome = "${androidExtension.sdkDirectory}"
-        } else {
-            throw new IllegalStateException('The android plugin is not exposing the SDK folder in an expected way.')
-        }
-        androidHome
+    public void testExtension() {
+        def extension = project.emulator as EmulatorExtension
+        def avd = new Avd()
+        extension.avd = avd
     }
 }
